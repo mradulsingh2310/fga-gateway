5f7c0383900208df6bcd3aeb5afc6735353bcd14
Mradul Singh <msingh@dealmeridian.com>
2026-07-07 02:41:11 +0530

    Add gateway Spring project shell

---
+++ src/main/java/io/agentctl/fgagateway/FgaGatewayApplication.java
@@ -0,0 +1,11 @@
+package io.agentctl.fgagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgaGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FgaGatewayApplication.class, args);
+    }
+}
